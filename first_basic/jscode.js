d58324ad919ee553fdce236a13ab5492a781b7d2
Thomas Olaszki <olaszky@gmail.com>
2018-10-02 09:58:31 +0200

    first basic ex completed

---
+++ first_basic/jscode.js
@@ -0,0 +1,11 @@
+$("#figure1").on("click", function() {
+   $("#message p").text("First Figure Clicked");
+})
+
+$("#figure2").on("click", function() {
+   $("#message p").text("Second Figure Clicked");
+})
+
+$("#figure3").on("click", function() {
+   $("#message p").text("Third Figure Clicked");
+})
